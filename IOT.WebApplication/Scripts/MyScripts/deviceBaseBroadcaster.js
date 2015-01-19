11b84a7f56bca1b98c87e81e07c3d510f7f99977
Mariusz Zieja <mariuszzieja@cybercom.com>
2015-01-19 13:34:08 +0100

    added datamse backup

--- IOT.WebApplication/Scripts/MyScripts/deviceBaseBroadcaster.js
+++ IOT.WebApplication/Scripts/MyScripts/deviceBaseBroadcaster.js
@@ -41,4 +41,39 @@
         })
     });
 
+    var styleCache = {};
+    var geoLayer = new ol.layer.Vector({
+        source: new ol.source.GeoJSON({
+            projection: 'EPSG:900913',
+            url: '/Home/GetMyGeoJson'
+        }),
+        style: function (feature, resolution) {
+            var text = resolution < 5000 ? feature.get('name') : '';
+            if (!styleCache[text]) {
+                styleCache[text] = [new ol.style.Style({
+                    fill: new ol.style.Fill({
+                        color: 'rgba(255, 255, 255, 0.1)'
+                    }),
+                    stroke: new ol.style.Stroke({
+                        color: '#319FD3',
+                        width: 1
+                    }),
+                    text: new ol.style.Text({
+                        font: '12px Calibri,sans-serif',
+                        text: text,
+                        fill: new ol.style.Fill({
+                            color: '#000'
+                        }),
+                        stroke: new ol.style.Stroke({
+                            color: '#fff',
+                            width: 3
+                        })
+                    }),
+                    zIndex: 999
+                })];
+            }
+            return styleCache[text];
+        }
+    });
+    map.addLayer(geoLayer);
 });
