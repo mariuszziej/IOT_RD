feb072a8cf14effc1a5f6bc7ba9133d476838e09
Mariusz Zieja <mariuszzieja@cybercom.com>
2015-01-21 09:54:20 +0100

    new logic for broadcasting devices [not working yet]

--- IOT.WebApplication/Scripts/MyScripts/deviceBaseBroadcaster.js
+++ IOT.WebApplication/Scripts/MyScripts/deviceBaseBroadcaster.js
@@ -1,5 +1,4 @@
 ﻿$(function () {
-
     var ticker = $.connection.deviceBaseBroadcasterHub; // the generated client-side hub proxy
 
     function init() {
@@ -9,79 +8,92 @@
                 console.log(device);
             });
         });
-    }
-
+    };
     // Add a client-side hub method that the server will call
     ticker.client.updateDevices = function (devices) {
-        $.each(devices, function () {
-            var device = (this);
-            console.log("new device:");
-            console.log(device);
-        });
-    }
+        populateChanges(devices);
+    };
 
     // Start the connection
     $.connection.hub.start().done(init);
 
-    var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-              source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-            center: ol.proj.transform([19.4694176, 51.7624200], 'EPSG:4326', 'EPSG:3857'),
-            zoom: 4
-        }),
-        controls: ol.control.defaults({
-            attributionOptions: {
-                collapsible: false
-            }
-        })
-    });
+    map = L.map('map').setView([51.7623, 19.4693], 13);
 
-    var styleCache = {};
-    var geoLayer = new ol.layer.Vector({
-        source: new ol.source.GeoJSON({
-            projection: 'EPSG:900913',
-            url: '/Home/GetMyGeoJson'
-        }),
-        style: function (feature, resolution) {
-            console.log(resolution);
-            if (resolution < 5000)
-            {
-                text = feature.get('name');
-            } else {
-                text = '';
-            }
-            if (!styleCache[text]) {
-                styleCache[text] = [new ol.style.Style({
-                    fill: new ol.style.Fill({
-                        color: 'rgba(255, 255, 255, 0.1)'
-                    }),
-                    stroke: new ol.style.Stroke({
-                        color: '#319FD3',
-                        width: 1
-                    }),
-                    text: new ol.style.Text({
-                        font: '12px Calibri,sans-serif',
-                        text: text,
-                        fill: new ol.style.Fill({
-                            color: '#000'
-                        }),
-                        stroke: new ol.style.Stroke({
-                            color: '#fff',
-                            width: 3
-                        })
-                    }),
-                    zIndex: 999
-                })];
-            } else {
-                var int = 0;
-            }
-            return styleCache[text];
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'examples.map-i875mjb7'
+    }).addTo(map);
+    map.addLayer(Markers);
+
+});
+
+var Markers = new L.FeatureGroup();
+var map;
+
+var populateChanges = function (devices) {
+    //map.removeLayer(Markers);
+    //Markers.clearLayers();
+    var markers = Markers.getLayers();
+    
+    $.each(devices, function () {
+        var device = (this);
+        var deviceIndex = isMarkersContainsDevice(markers, device);
+        if (deviceIndex != -1) {
+            var coordinates = JSON.parse(device.Message);
+            var newLatLng = new L.LatLng(coordinates.latitude, coordinates.longitude);
+            markers[deviceIndex].setLatLng(newLatLng);
+            markers[deviceIndex].EventDate = device.EventDate;
+            markers[deviceIndex].Message = device.Message;
+            markers[deviceIndex].IMessageTypeId = device.IMessageTypeId;
+            markers[deviceIndex].unbindPopup();
+            markers[deviceIndex].bindPopup(createPopupFromDevice(device));
+        } else {
+            var coordinates = JSON.parse(device.Message);
+            var marker = L.marker([coordinates.latitude, coordinates.longitude]);
+            marker.bindPopup(createPopupFromDevice(device));
+            extendMarkerByDeviceProperties(marker, device);
         }
     });
-    map.addLayer(geoLayer);
-});
+    //map.addLayer(Markers);
+}
+
+var extendMarkerByDeviceProperties = function (marker, device) {
+    marker.DeviceTypeId = device.DeviceTypeId
+    marker.DeviceId = device.DeviceId
+    marker.MessageTypeId = device.IMessageTypeId;
+    marker.Message = device.Message;
+    marker.EventDate = device.EventDate;
+    marker.Id = device.Id;
+}
+
+var createPopupFromDevice = function (device) {
+    var returnPopup = "<p>DeviceTypeId:" + device.DeviceTypeId + "</p>" +
+                      "<p>DeviceId:" + device.DeviceId + "</p>" +
+                      "<p>MessageTypeId:" + device.MessageTypeId + "</p>" +
+                      "<p>Message:" + device.Message + "</p>" +
+                      "<p>EventDate:" + device.EventDate + "</p>" +
+                      "<p>ID:" + device.Id + "</p>";
+
+    return returnPopup;
+        
+};
+
+var isMarkersContainsDevice = function (markers, device) {
+    for (var i = 0; i < markers.length; i++) {
+        if (markers[i].Id == device.Id) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+//kind of reflection in JavaScript
+var getPropertyTable = function (obj) {
+    var keys = [];
+    for (var key in obj) {
+        keys.push(key);
+    }
+    return keys;
+}
